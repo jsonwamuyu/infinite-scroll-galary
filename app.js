7be5b0c4b1be46b26adb0ee995918c8908d6725d
jsonwamuyu <jysonmuchiri@gmail.com>
2025-01-16 02:44:13 -0800

    Add files and more images

---
+++ app.js
@@ -0,0 +1,24 @@
+// Array of images
+const images = [
+    { imgUrl: './images/1.jpg', id: 1, altText: "one" },
+    { imgUrl: './images/1.jpg', id: 2, altText: "two" },
+]
+
+// get the container to hold the images
+const myGallary = document.getElementById('myGallary')
+console.log(myGallary);
+
+
+// load the images
+const loadTenImages = ()=>{
+
+}
+
+// load images the first time
+loadTenImages()
+
+
+// add scrolling even listener to check when the scrolling is almost at the bottom
+
+
+
